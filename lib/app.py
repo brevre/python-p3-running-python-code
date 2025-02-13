01cc546b02908b0b27e7bca1ca50bdc4e4a48d0d
brevre <brendamakena09@gmail.com>
2025-02-13 14:08:02 +0300

    assgn completed

--- lib/app.py
+++ lib/app.py
@@ -1,7 +1,10 @@
+<<<<<<< HEAD
 # print("Hello world!", end=" ")
 # print("Hello sun!", end="!! ")
 # print("Hello sky!", end="!!!\n")
 
 
 
+=======
+>>>>>>> 451611f (assgn completed)
 print("Hello World! Pass this test, please.")
